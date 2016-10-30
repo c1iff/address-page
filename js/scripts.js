704747bda85c1c37fb5096fb93c7fab9265cc364
Ryan Peterson <ryanpeterson08@gmail.com>
2016-10-30 16:09:13 -0700

    add contact constructor and jquery to display first and last name

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function Contact(first, last){
+  this.firstName = first;
+  this.lastName = last;
+}
+
+$(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").show();
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    event.preventDefault();
+  });
+});
